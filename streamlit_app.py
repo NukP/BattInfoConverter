5173a297a2c66c82cafa6900399771daa5032af3
NukP <nukorn.p27@gmail.com>
2024-06-24 15:45:00 +0200

    Add example files link

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,9 @@ BattINFO converter helps you ontologize metadata for coin cell battery based on 
 ## Excel metadata files
 Here you will find an Excel file template that you can fill out with your metadata. We will add more template for other cell types in the future. Be sure to check out!
 ### Blank Excel metadata file
-[Coin cell battery schemas version 0.1.0](https://drive.switch.ch/index.php/s/4S9Ft8hwNz31YMQ) 
+[Coin cell battery schemas version 0.1.0](https://raw.githubusercontent.com/NukP/xls_convert/fix_oslo2/Excel%20for%20reference/CoinCellBattery_Schemas_version_010.xlsx) 
 ### Example filled out Excel metadata file
+[Example-filled Coin cell battery schemas version 0.1.0](https://raw.githubusercontent.com/NukP/xls_convert/fix_oslo2/Excel%20for%20reference/Example_CoinCellBattery_Schemas_version_010.xlsx)
 
  ## How to Fill Out the Excel metadata file
 
