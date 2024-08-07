d42143a3952df59300516325aa24ac3bf6ebdaa5
NukP <nukorn.p27@gmail.com>
2024-08-07 17:22:21 +0200

    update plf function

--- auxiliary.py
+++ auxiliary.py
@@ -16,53 +16,53 @@ def add_to_structure(jsonld, path, value, unit, data_container):
             is_second_last = idx == len(path) - 2
 
             if part not in current_level:
-                plf(value)
+                plf(value, part)
                 if part in connectors:
-                    plf(value)
+                    plf(value, part)
                     connector_type = context_connector.loc[context_connector['Item'] == part, 'Key'].values[0]
                     if pd.isna(connector_type):
-                        plf(value)
+                        plf(value, part)
                         current_level[part] = {}
                     else:
-                        plf(value)
+                        plf(value, part)
                         current_level[part] = {"@type": connector_type}
                 else:
-                    plf(value)
+                    plf(value, part)
                     current_level[part] = {}
             
             #Handle the case of the single path.
             if len(path) == 1 and unit == 'No Unit':
-                plf(value)
+                plf(value, part)
                 if value in unique_id['Item'].values:
-                    plf(value)
+                    plf(value, part)
                     if "@type" in current_level:
-                        plf(value)
+                        plf(value, part)
                         if "@type" in current_level[part] and isinstance(current_level[part]["@type"], list):
-                            plf(value)
+                            plf(value, part)
                             if not pd.isna(value):
-                                plf(value)
+                                plf(value, part)
                                 current_level[part]["@type"].append(value)
                         else:
-                            plf(value)
+                            plf(value, part)
                             if not pd.isna(value):
-                                plf(value)
+                                plf(value, part)
                                 current_level[part]["@type"] = [value]
                     else:
-                        plf(value)
+                        plf(value, part)
                         if not pd.isna(value):
-                            plf(value)
+                            plf(value, part)
                             current_level[part]["@type"] = value
                 else:
-                    plf(value)
+                    plf(value, part)
                     current_level[part]['rdfs:comment'] = value
                 break
 
             if is_second_last and unit != 'No Unit':
-                plf(value)
+                plf(value, part)
                 if pd.isna(unit):
-                    plf(value)
+                    plf(value, part)
                     raise ValueError(f"The value '{value}' is filled in the wrong row, please check the schema")
-                unit_info = unit_map[unit] ; plf(value)
+                unit_info = unit_map[unit] ; plf(value, part)
 
                 new_entry = {
                     "@type": path[-1],
@@ -75,97 +75,100 @@ def add_to_structure(jsonld, path, value, unit, data_container):
                 
                 # Check if the part already exists and should be a list
                 if part in current_level:
-                    plf(value)
+                    plf(value, part)
                     if isinstance(current_level[part], list):
-                        current_level[part].append(new_entry) ;plf(value)
+                        current_level[part].append(new_entry) ;plf(value, part)
                     else:
                         # Ensure we do not overwrite non-dictionary values
-                        existing_entry = current_level[part] ;plf(value)
+                        existing_entry = current_level[part] ;plf(value, part)
                         current_level[part] = [existing_entry, new_entry]
                 else:
-                    current_level[part] = [new_entry] ;plf(value)
+                    current_level[part] = [new_entry] ;plf(value, part)
                 
                 # Clean up any empty dictionaries in the list
                 if isinstance(current_level[part], list):
-                    current_level[part] = [item for item in current_level[part] if item != {}] ;plf(value)
+                    current_level[part] = [item for item in current_level[part] if item != {}] ;plf(value, part)
                 break
 
             if is_last and unit == 'No Unit':
-                plf(value)
+                plf(value, part)
                 if value in unique_id['Item'].values:
-                    plf(value)
+                    plf(value, part)
                     if "@type" in current_level:
-                        plf(value)
+                        plf(value, part)
                         if isinstance(current_level["@type"], list):
-                            plf(value)
+                            plf(value, part)
                             if not pd.isna(value):
-                                current_level["@type"].append(value) ; plf(value)
+                                current_level["@type"].append(value) ; plf(value, part)
                         else:
-                            plf(value)
+                            plf(value, part)
                             if not pd.isna(value):
-                                current_level["@type"] = [current_level["@type"], value] ; plf(value)
+                                current_level["@type"] = [current_level["@type"], value] ; plf(value, part)
                     else:
                         if not pd.isna(value):
-                            current_level["@type"] = value ; plf(value)
+                            current_level["@type"] = value ; plf(value, part)
                 else:
-                    current_level['rdfs:comment'] = value ; plf(value)
+                    current_level['rdfs:comment'] = value ; plf(value, part)
                 break
 
             current_level = current_level[part]
 
             if not is_last and part in connectors:
-                connector_type = context_connector.loc[context_connector['Item'] == part, 'Key'].values[0] ; plf(value)
+                connector_type = context_connector.loc[context_connector['Item'] == part, 'Key'].values[0] ; plf(value, part)
                 if not pd.isna(connector_type):
-                    plf(value)
+                    plf(value, part)
                     if "@type" not in current_level:
-                        current_level["@type"] = connector_type ; plf(value)
+                        current_level["@type"] = connector_type ; plf(value, part)
                     elif current_level["@type"] != connector_type:
-                        plf(value)
+                        plf(value, part)
                         if isinstance(current_level["@type"], list):
-                            plf(value)
+                            plf(value, part)
                             if connector_type not in current_level["@type"]:
-                                current_level["@type"].append(connector_type) ; plf(value)
+                                current_level["@type"].append(connector_type) ; plf(value, part)
                         else:
-                            current_level["@type"] = [current_level["@type"], connector_type] ; plf(value)
+                            current_level["@type"] = [current_level["@type"], connector_type] ; plf(value, part)
 
             if is_second_last and unit == 'No Unit':
-                next_part = path[idx + 1] ; plf(value)
+                next_part = path[idx + 1] ; plf(value, part)
                 if isinstance(current_level, dict): 
-                    plf(value)
+                    plf(value, part, current_level=current_level)
                     if next_part not in current_level:
-                        current_level[next_part] = {} ; plf(value)
+                        current_level[next_part] = {} ; plf(value, part)
                     current_level = current_level[next_part]
                 elif isinstance(current_level, list):
-                    current_level.append({next_part: {}}) ; plf(value)
+                    current_level.append({next_part: {}}) ; plf(value, part)
                     current_level = current_level[-1][next_part]
 
                 if value in unique_id['Item'].values:
-                    unique_id_of_value = get_information_value(unique_id, 'ID', value, "Item") ; plf(value)
+                    unique_id_of_value = get_information_value(unique_id, 'ID', value, "Item") ; plf(value, part)
                     if not pd.isna(unique_id_of_value):
-                        current_level['@id'] = unique_id_of_value ; plf(value)
+                        current_level['@id'] = unique_id_of_value ; plf(value, part)
                     
                     if not pd.isna(value):
-                        plf(value)
+                        plf(value, part, current_level=current_level)
                         if "@type" in current_level:
-                            plf(value)
+                            plf(value, part)
                             if isinstance(current_level["@type"], list):
-                                current_level["@type"].append(value) ; plf(value)
+                                current_level["@type"].append(value) ; plf(value, part)
                             else:
-                                current_level["@type"] = [current_level["@type"], value] ; plf(value)
+                                current_level["@type"] = [current_level["@type"], value] ; plf(value, part)
                         else:
-                            current_level["@type"] = value ; plf(value)
+                            current_level["@type"] = value ; plf(value, part)
                 else:
-                    current_level['rdfs:comment'] = value ; plf(value)
+                    current_level['rdfs:comment'] = value ; plf(value, part)
                 break
 
     except Exception as e:
         traceback.print_exc()  # Print the full traceback
         raise RuntimeError(f"Error occurred with value '{value}' and path '{path}': {str(e)}")
     
-def plf(value, current_level_part = None):
+def plf(value, part, current_level = None):
+    """Print Line Function.
+    This function is used for debugging.
+    """
     current_frame = inspect.currentframe()
     line_number = current_frame.f_back.f_lineno
-    if current_level_part is not None:
-        print(f'pass line {line_number}, value:', value,'AND current_level_part:', current_level_part)
+    if current_level is not None:
+        print(f'pass line {line_number}, value:', value,'AND part:', part, 'AND current_level:', current_level)
     else:
-        print(f'pass line {line_number}, value:', value)
+        print(f'pass line {line_number}, value:', value,'AND part:', part)
