d5af16672201e44a98431708bf154fd3aee287ba
NukP <nukorn.p27@gmail.com>
2024-08-09 13:58:35 +0200

    Preparing main page for multiple pages format

---
+++ Main_page.py
@@ -0,0 +1,101 @@
+"""
+This module handle the interface of the web app. 
+"""
+import streamlit as st
+import json
+import os
+from io import BytesIO
+import json_convert as js_conv 
+
+
+markdown_content = """ 
+Ontologizing your metadata can significantly enhance the interoperability of data across various research groups.
+To learn more about ontologizing your metadata, we invite you to visit our page on
+[ontologizing metadata](https://github.com/ord-premise/interoperability-guidelines/tree/main). 
+While the benefits of this process are clear, it can often be a daunting task in practice. 
+With this in mind, we've developed an BattINFO converter web application designed to streamline and expedite this intricate task,
+making it more manageable for you and your team.
+
+BattINFO converter helps you ontologize metadata for coin cell battery based on [BattINFO](https://big-map.github.io/BattINFO/index.html) ontology. 
+
+## Excel metadata files
+Here you will find an Excel file template that you can fill out with your metadata. We will add more template for other cell types in the future. Be sure to check out!
+### Blank Excel metadata file
+[Coin cell battery schema version 0.1.0](https://raw.githubusercontent.com/NukP/xls_convert/fix_oslo2/Excel%20for%20reference/CoinCellBattery_Schemas_version_010.xlsx) 
+### Example filled out Excel metadata file
+[Example-filled Coin cell battery schema version 0.1.0](https://raw.githubusercontent.com/NukP/xls_convert/fix_oslo2/Excel%20for%20reference/Example_CoinCellBattery_Schemas_version_010.xlsx)
+
+ ## How to Fill Out the Excel metadata file
+
+1. All the tabs present in the Excel files are essential for the web app. __Do not change their names__.  
+2. **First Row**: Ensure that the first row of each tab contains the column names. These names are used as references by the app.  
+3. **Filling Data**: In the `Schema` tab, fill in the following columns:  
+    - **Value**: Enter the metadata value. If the cell is empty, the script will skip this metadata item.  
+    - **Unit**: Specify the unit of the metadata. If the metadata item doesn't require a unit, enter "No Unit". Leaving this cell empty will cause an error.  
+    - **Ontology link**: Provide the ontology link. If you do not want to ontologize a particular row, enter "NotOntologize". To add a comment instead, enter "Comment".  
+
+## How to adapt the Excel metadata file for your own custom data
+
+
+1. **Adding New Ontology Terms**:
+    - Go to the website providing the ontology concept (e.g., BattInfo, Schema.org).
+    - In the `@context-TopLevel` tab, add the top-level URL of the ontology website.
+    - In the `Ontology - Item` tab, define the new ontology term. Use the `Item` column for the term and the `Key` column for the ontology reference (e.g., `schemas:identifier`).
+
+2. **Adding New Units**:
+    - In the `Ontology - Unit` tab, define the new unit. Include the `Item`, `Label`, and `Symbol` columns, along with the ontology reference in the `Key` column.
+
+3. **Adding New Rows**:  
+    - Add new rows to the `Schema` tab (or remove existing rows) to include additional metadata items. Ensure that the `Ontology link` column references the new ontology terms and units.
+
+# Acknowledgement 
+BattINFO converter web application was developed at [Empa](https://www.empa.ch/), Swiss Federal Laboratories for Materials Science and Technology in [Material for Energy Conversion lab](https://www.empa.ch/web/s501).  
+
+
+We acknowledge stimulating discussions and support from Dr Simon Clark, SINTEF, Norway.
+
+
+This work has been developed under the following project and funding agencies: 
+- [PREMISE](https://ord-premise.org/)  
+  
+- [Battery 2030+](https://battery2030.eu/)
+
+- [Big-Map project](https://www.big-map.eu/)
+
+
+"""
+
+image_url = 'https://raw.githubusercontent.com/NukP/xls_convert/fix_oslo2/BattInfoCOnverter.png'
+
+def main():
+    st.image(image_url)
+    
+    st.markdown(f"__App Version: {js_conv.APP_VERSION}__")
+    
+    uploaded_file = st.file_uploader("__Upload your metadata Excel file here__", type=['xlsx', 'xlsm'])
+    
+    if uploaded_file is not None:
+        # Extract the base name of the file (without the extension)
+        base_name = os.path.splitext(uploaded_file.name)[0]
+        
+        # Convert the uploaded Excel file to JSON-LD
+        jsonld_output = js_conv.convert_excel_to_jsonld(uploaded_file)
+        jsonld_str = json.dumps(jsonld_output, indent=4)
+
+        # Download button
+        to_download = BytesIO(jsonld_str.encode())
+        output_file_name = f"BattINFO_converter_{base_name}.json"  
+        st.download_button(label="Download JSON-LD",
+                        data=to_download,
+                        file_name=output_file_name,
+                        mime="application/json")
+        
+        # Convert JSON-LD output to a string to display in text area (for preview)
+        st.text_area("JSON-LD Output", jsonld_str, height=1000)
+    
+    st.markdown(markdown_content, unsafe_allow_html=True)
+    st.image('https://raw.githubusercontent.com/NukP/xls_convert/fix_oslo2/sponsor.png', width=700)
+
+if __name__ == "__main__":
+    main()
+
